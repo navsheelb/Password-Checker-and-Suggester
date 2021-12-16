d1a4683ca0252a1fd85f3777b501ef2d3bea9747
Navsheel Biral <55994251+navsheelb@users.noreply.github.com>
2021-12-17 02:08:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,140 @@
+
+        const indicator = document.querySelector(".indicator");
+         const input = document.querySelector("input");
+         const weak = document.querySelector(".weak");
+         const medium = document.querySelector(".medium");
+         const strong = document.querySelector(".strong");
+         const text = document.querySelector(".text");
+         const showBtn = document.querySelector(".showBtn");
+         let regExpWeak = /[a-z]/;
+         let regExpMedium = /\d+/;
+         let regExpStrong = /.[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/;
+         function trigger(){
+           if(input.value != ""){
+             indicator.style.display = "block";
+             indicator.style.display = "flex";
+             if(input.value.length <= 3 && (input.value.match(regExpWeak) || input.value.match(regExpMedium) || input.value.match(regExpStrong)))no=1;
+             if(input.value.length >= 6 && ((input.value.match(regExpWeak) && input.value.match(regExpMedium)) || (input.value.match(regExpMedium) && input.value.match(regExpStrong)) || (input.value.match(regExpWeak) && input.value.match(regExpStrong))))no=2;
+             if(input.value.length >= 6 && input.value.match(regExpWeak) && input.value.match(regExpMedium) && input.value.match(regExpStrong))no=3;
+             if(no==1){
+               weak.classList.add("active");
+               text.style.display = "block";
+               text.textContent = "Your password is weak 😥";
+               text.classList.add("weak");
+             }
+             if(no==2){
+               medium.classList.add("active");
+               text.textContent = "Your password is okay 👍";
+               text.classList.add("medium");
+             }else{
+               medium.classList.remove("active");
+               text.classList.remove("medium");
+             }
+             if(no==3){
+               weak.classList.add("active");
+               medium.classList.add("active");
+               strong.classList.add("active");
+               text.textContent = "Your password is strong 💪";
+               text.classList.add("strong");
+             }else{
+               strong.classList.remove("active");
+               text.classList.remove("strong");
+             }
+             showBtn.style.display = "block";
+             showBtn.onclick = function(){
+               if(input.type == "password"){
+                 input.type = "text";
+                 showBtn.textContent = "HIDE";
+                 showBtn.style.color = "#23ad5c";
+               }else{
+                 input.type = "password";
+                 showBtn.textContent = "SHOW";
+                 showBtn.style.color = "#000";
+               }
+             }
+           }else{
+             indicator.style.display = "none";
+             text.style.display = "none";
+             showBtn.style.display = "none";
+           }
+         }
+
+         const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboard = document.getElementById('clipboard');
+
+const randomFunc = {
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol
+}
+
+clipboard.addEventListener('click', () => {
+	const textarea = document.createElement('textarea');
+	const password = resultEl.innerText;
+	
+	if(!password) { return; }
+	
+	textarea.value = password;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand('copy');
+	textarea.remove();
+	alert('Password Copied to Clipboard');
+});
+
+generateEl.addEventListener('click', () => {
+	const length = +lengthEl.value;
+	const hasLower = lowercaseEl.checked;
+	const hasUpper = uppercaseEl.checked;
+	const hasNumber = numbersEl.checked;
+	const hasSymbol = symbolsEl.checked;
+	
+	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+	let generatedPassword = '';
+	const typesCount = lower + upper + number + symbol;
+	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+	
+	// Doesn't have a selected type
+	if(typesCount === 0) {
+		return 'Select atleast 1 option';
+	}
+	
+	// create a loop
+	for(let i=0; i<length; i+=typesCount) {
+		typesArr.forEach(type => {
+			const funcName = Object.keys(type)[0];
+			generatedPassword += randomFunc[funcName]();
+		});
+	}
+	
+	const finalPassword = generatedPassword.slice(0, length);
+	
+	return finalPassword;
+}
+
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.'
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
